2ddb2ba2ef04b21d5129d1d047511b8af57ea535
Rashed967 <rashedhasan967.com>
2023-05-01 22:00:03 +0600

    just added files

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB3YEeLknubqMM0BDhSlEhvcCnxqhn3LCE",
+  authDomain: "afghan-chef-hunter-by-rashed.firebaseapp.com",
+  projectId: "afghan-chef-hunter-by-rashed",
+  storageBucket: "afghan-chef-hunter-by-rashed.appspot.com",
+  messagingSenderId: "209064770182",
+  appId: "1:209064770182:web:f3a8d6fcf2ec320537ef07"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
