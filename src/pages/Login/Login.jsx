14ec78fc2f9c16781d1daf48950feaa95d34c000
Rashed967 <rashedhasan967.com>
2023-05-02 18:42:54 +0600

    google sign in done

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { FaExpeditedssl, FaGithub, FaGoogle } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../providers/AuthProvider/AuthProvider';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const auth = getAuth();
+
 
 const Login = () => {
+  const {signInUser, googelSignIn} = useContext(AuthContext)
+
+  const handleSignIn = (event) => {
+    event.preventDefault()
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value; 
+    signInUser(email, password)
+    .then(result => {
+      const loggedUser = result.user;
+      console.log(loggedUser)
+    })
+    .catch(error => {
+      console.error(error.message)
+    })
+    console.log(email, password)
+  } 
+
+ 
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
@@ -12,12 +36,12 @@ const Login = () => {
     </div>
 
     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-      <div className="card-body">
+      <form onSubmit={handleSignIn} className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="text" placeholder="email" className="input input-bordered" />
+          <input type="text" placeholder="email" name='email' className="input input-bordered" />
         </div>
 
 
@@ -25,7 +49,7 @@ const Login = () => {
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="text" placeholder="password" className="input input-bordered" />
+          <input type="text" placeholder="password" name='password' className="input input-bordered" />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
@@ -39,11 +63,11 @@ const Login = () => {
         <div>
             <h3 className='text-center border-b-2'>Other option</h3>
             <div className='flex justify-center space-x-3 text-2xl mt-4'>
-                <span><FaGoogle /></span>
+                <span onClick={googelSignIn}><FaGoogle /></span>
                 <span><FaGithub /></span>
             </div>
         </div>
-      </div>
+      </form>
     </div>
   </div>
 </div>
--- src/providers/AuthProvider/AuthProvider.jsx
+++ src/providers/AuthProvider/AuthProvider.jsx
@@ -1,9 +1,11 @@
 import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import app from '../../firebase/firebase.config';
+import {  signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 export const AuthContext = createContext()
 const auth =getAuth(app)
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null)
@@ -12,13 +14,44 @@ const AuthProvider = ({children}) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
+    const signInUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const googelSignIn = () =>{
+        signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+    
+      }
+
+    
+
     // const user = {
     //     displayName : "Rashed Hasan"
     // }
 
     const authInfo = {
         user,
-        createUser
+        createUser,
+        signInUser,
+        googelSignIn
     }
     return (
         <AuthContext.Provider value={authInfo}>
