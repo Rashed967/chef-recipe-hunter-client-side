e3051641e681c7898113a06c9cf7f828e92fdf8a
Rashed967 <rashedhasan967.com>
2023-05-04 16:54:44 +0600

    ratting added

--- src/pages/ChefRecipes/Recipe/Recipe.jsx
+++ src/pages/ChefRecipes/Recipe/Recipe.jsx
@@ -4,8 +4,11 @@ import { FaHeart } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
   import 'react-toastify/dist/ReactToastify.css';
+  import { Rating } from '@smastrom/react-rating';
 
+  import '@smastrom/react-rating/style.css'
 const Recipe = ({recipe}) => {
+  const [rating, setRating] = useState(Math.round(Math.random() * 5))
     const {ingredients} = recipe;
     const [disable, setDisable] = useState(false)
     const notify = () => toast.success('Recipe added', {
@@ -40,12 +43,13 @@ const Recipe = ({recipe}) => {
                   }
                   <p><span className='font-semibold text-xl'>Cooking methode :</span> {recipe?.method}</p>
                   <div className="card-actions justify-between items-center mt-3">
+                    <Rating style={{ maxWidth: 120 }} value={rating} readOnly />
                     <div className="flex space-x-2 items-center ">
                     <button disabled={disable} onClick={likeHandler} className={`text-2xl cursor-pointer ${disable ? "text-red-600" : "" } `}><FaHeart  ></FaHeart></button>
 
-                    {/* <div className={`text-2xl cursor-pointer ${disable ? "text-red-600" : ""}`}>{children}</div> */}
+                   
                  </div>
-                    {/* <Link to={`/chefs/${id}`} className="badge badge-outline">View Recipes</Link> */}
+         
                </div>
                  </div>
                  <ToastContainer />
